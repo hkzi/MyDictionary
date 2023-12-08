012d303e6733c8c0e647d4b0384a37e2954f7b81
hdzi <hdzi@DESKTOP-N5TJS0C>
2023-12-08 20:15:45 +0300

    Add project files.

---
+++ MyDictionary/MyList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    public class MyList<T>
+    {
+        private T[] values;
+        public MyList()
+        {
+            values = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = values;
+            values = new T[values.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                values[i] = tempArray[i];
+            }
+            values[values.Length - 1] = item;
+        }
+
+        public int Count
+        {
+            get { return values.Length; } 
+
+        }
+    }
+}
+
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> myList = new MyList<string>();
+            myList.Add("Ankara");
+            myList.Add("İzmir");
+            myList.Add("Sivas");
+            Console.WriteLine("myList eleman sayısı : {0}",myList.Count);
+
+            MyList<string> myList2 = new MyList<string>();
+            myList2.Add("İstanbul");
+            myList2.Add("Adana");
+            Console.WriteLine("myList2 eleman sayısı : {0}",myList2.Count);
+            Console.ReadLine();
+
+
+        }
+    }
+}
